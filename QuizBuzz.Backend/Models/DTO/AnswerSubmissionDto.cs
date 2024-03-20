f510284cc326fcef7b6982e90dd6e90e76603336
YanivShahar <yanivshahar97@gmail.com>
2024-03-20 19:26:51 +0200

    Added AnswerSubmissionDto

---
+++ QuizBuzz.Backend/Models/DTO/AnswerSubmissionDto.cs
@@ -0,0 +1,12 @@
+﻿namespace QuizBuzz.Backend.Models.DTO
+{
+    public class AnswerSubmissionDto
+    {
+        public string SessionId { get; set; } = String.Empty;
+        public string Nickname { get; set; } = String.Empty;
+        public int QuestionIndex { get; set; } = -1;
+
+        // The `required` modifier ensures that this property must be initialized
+        public required QuestionResponse QuestionResponse { get; set; }
+    }
+}
